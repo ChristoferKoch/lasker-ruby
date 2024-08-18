8d434ff9055e84eaae187b60ec109fbb6f1b43da
Christofer Koch <cbkoch13@gmail.com>
2024-08-17 21:42:53 -0700

    feat: add piece class helper functions

---
+++ lib/pieces.rb
@@ -0,0 +1,32 @@
+class Piece
+  # Useful for debugging
+  def display_bitboard(bitboard)
+    bitboard = convert_to_64(bitboard)
+    index = 0
+    rank = 8
+
+    while rank > 0
+      spaced_line = line_spacing(bitboard[index, 8])
+      puts "#{rank} #{spaced_line}"
+      index += 8
+      rank -= 1
+    end
+
+    puts "  abcdefgh"
+  end
+
+  # Create a string to give the binary
+  # equivelent of an unsigned long long in C
+  def convert_to_64(bitboard)
+    bitboard = bitboard.to_s(2)
+    difference = 64 - bitboard.length
+    leading_bits = ''
+
+    while difference > 0
+      leading_bits += '0'
+      difference -= 1
+    end
+
+    return leading_bits + bitboard
+  end
+end
