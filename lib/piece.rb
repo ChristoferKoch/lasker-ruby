43e4e649f27028dfb1ff8dcbb31a7c70915bf07e
Christofer Koch <cbkoch13@gmail.com>
2024-10-12 13:16:29 -0700

    feat: pinned moves can move along attack path

--- lib/piece.rb
+++ lib/piece.rb
@@ -15,23 +15,27 @@ class Piece
 
   # Works only for sliding pieces. Knights, pawns, and kings need unique functions
   def moves(same_occupancy, diff_occupancy, opp_pieces, king)
-    moves = 0
+    moves = []
     indicies = get_indicies
     indicies.each do |index|
-      if !pinned?(same_occupancy | diff_occupancy, opp_pieces, king, index)
-        moveboard = move_mask(1 << index)
+      moveboard = move_mask(1 << index)
+      pin_check = pinned(same_occupancy | diff_occupancy, opp_pieces, king, index)
+      if !pin_check
         same_blockerboard = moveboard & same_occupancy
         diff_blockerboard = moveboard & diff_occupancy
         blockerboard = same_blockerboard | diff_blockerboard
-        moves |= blockerboard > 0 ? unblocked_moves(same_blockerboard, diff_blockerboard) : moveboard
-        moves |= king.in_check ? moves & king.checkboard : moves
+        moveboard = blockerboard > 0 ? unblocked_moves(same_blockerboard, diff_blockerboard, moveboard) : moveboard
+        moveboard = king.in_check ? moveboard & king.checkboard : moveboard
+      elsif moveboard && king.bitboard > 0
+        moveboard |= get_ray(king.bitboard, index) ^ king.bitboard
+        moveboard = king.in_check ? 0 : pin_check | moveboard
       end
     end
-    return moves
+    return encode_moves(moves)
   end
 
   # Determines if piece is pinned
-  def pinned?(occupancy, attackers, king, index)
+  def pinned(occupancy, attackers, king, index)
     attackers.each_value do |piece|
       if piece.is_a?(Rook) || piece.is_a?(Bishop) || piece.is_a?(Queen)
         if piece.attackboard & king.bitboard > 0
@@ -40,13 +44,13 @@ class Piece
           if rayboard & pieceboard > 0
             rayboard = get_ray(piece.bitboard, index)
             if rayboard & occupancy == 0
-              return true
+              return rayboard
             end
           end
         end
       end
     end
-    return false
+    return nil
   end
 
 end
